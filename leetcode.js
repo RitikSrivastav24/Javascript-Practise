88ee7ec46f119e0720777526c1eb93b3cd385922
Ritik Shrivastav <ritiksri35@gmail.com>
2026-04-04 17:02:40 +0530

    Added leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -6,4 +6,31 @@ var functionArray= function(arr){
     }
     return result;
 }
-console.log(functionArray([1,2,3,4]))
+//console.log(functionArray([1,2,3,4]))
+
+function EvenNum(arr){
+    let result= [];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] % 2 ===0){
+            result.push(arr[i])
+        }
+    }
+    return result;
+}
+console.log(EvenNum([1,23,4,56]));
+
+
+// Q.3 Given an array and a function fn, return a new array where each element is transformed using fn.
+
+function givenArr(arr, fn){
+    let result=[];
+    for(let i =0 ; i<arr.length; i++){
+        result[i]= fn(arr[i], i);
+
+    }
+    return result;
+}
+console.log(givenArr([1,2,3,4], (x) => x* 2))
+
+let result=[1,2,3,4].map((x,i) => x*2);
+console.log(result)
