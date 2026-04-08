821e8038af7a1b152705427c9c4ff53ddfdceb3f
Ritik Shrivastav <ritiksri35@gmail.com>
2026-04-08 10:39:08 +0530

    Promises

--- Promises.js
+++ Promises.js
@@ -1,5 +1,3 @@
-
-
 const promiseOne= new Promise(function(resolve, reject){
     setTimeout(() => {
         console.log("Asynch task is complete!!")
@@ -33,7 +31,7 @@ promiseThree.then(function(user){
 // chaining
 const promiseFour= new Promise(function(resolve, reject){
     setTimeout(()=>{
-       let error= false;
+       let error= true;
        if(!error){
         resolve({username:"abc", password:"123"})
        }
@@ -51,3 +49,51 @@ promiseFour.then((user)=>{
 }).catch(function(error){
     console.log(error)
 }).finally(()=> console.log("Finally code runs"))
+
+
+const promiseFive= new Promise(function(resolve,reject){
+    setTimeout(() => {
+        let error=true;
+        if(!error){
+            resolve({username:"Javascript", password:123})
+        }
+        else{
+            reject("Error : JS went wrong!")
+        }
+        
+    }, 1000);
+})
+
+// consuming promiseFive using async & await
+
+ async function consumedPromiseFive() {
+    try {
+        const response= await promiseFive
+    console.log(response);
+        
+    } catch (error) {
+     console.log(error)   
+    }
+    
+}
+consumedPromiseFive()
+
+// async function getAllUsers() {
+//     const response= await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data= await response.json(); // we have to create await response.json also because it is taking time also so we have to wait fot this also.
+//     console.log(data)
+// }
+// getAllUsers()
+
+// writing this again by using then and catch
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch((error)=>{
+    console.log(error)
+})
