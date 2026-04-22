b25dfa70b802ae0896200125c0046056cddf50fb
Ritik Shrivastav <ritiksri35@gmail.com>
2026-04-22 16:42:45 +0530

    Solve Leetcode Question

--- leetcode.js
+++ leetcode.js
@@ -71,24 +71,24 @@ function memoize(fn) {
 */
 
 // Solution
-    var timeLimit = function (fn, t) {
-    
-        return async function (...args) {
-            return new Promise((resolve, reject) => {
-                const timer = setTimeout(() => {
-                    reject("Time Limit Exceeded")
-                }, t)
-    
-                fn(...args)
-                    .then((res) => {
-                        clearTimeout(timer)
-                        resolve(res)
-                    })
-                    .catch((err) => {
-                        clearTimeout(timer)
-                        reject(err)
-                    })
-    
-            })
-        }
-    };
+var timeLimit = function (fn, t) {
+
+    return async function (...args) {
+        return new Promise((resolve, reject) => {
+            const timer = setTimeout(() => {
+                reject("Time Limit Exceeded")
+            }, t)
+
+            fn(...args)
+                .then((res) => {
+                    clearTimeout(timer)
+                    resolve(res)
+                })
+                .catch((err) => {
+                    clearTimeout(timer)
+                    reject(err)
+                })
+
+        })
+    }
+};
